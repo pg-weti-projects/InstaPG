b6e7a4477e947320b72f8207bb0d68ad258d47a8
martabartsch <martabartsch@outlook.com>
2024-04-11 21:37:53 +0200

    Added basic app design and functionality

--- InstaPGClient/MainWindow.xaml.cs
+++ InstaPGClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,15 +29,69 @@ namespace InstaPGClient
         }
 
 
-        private void TestButton_Click(object sender, RoutedEventArgs e)
+        private void LogoutButton_Click(object sender, MouseButtonEventArgs e)
         {
             InstaPGServiceClient client = new InstaPGServiceClient();
+            try
+            {
+                string result = client.GetData(0);
 
-            var output = client.GetData(random.Next(1000));
-
-            TestTextBlock.Text = output;
+                MessageBox.Show("User has been logged out.", "Logged Out", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unexpected error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
             client.Close();
         }
+
+
+        private void AddAvatarButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    BitmapImage bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+                    UserAvatar.Source = bitmap;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An unexpected error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void AddPhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    Image newImage = new Image();
+                    BitmapImage bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+                    newImage.Source = bitmap;
+                    newImage.Width = 126;
+                    newImage.Height = 115;
+                    newImage.Margin = new Thickness(5);
+
+                    UserGallery.Items.Add(newImage);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An unexpected error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
     }
 }
