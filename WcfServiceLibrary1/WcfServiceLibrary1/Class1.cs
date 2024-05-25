f07c8cc352ceff1d4599d08d7a8856ef65388d2c
kamillmaziarz <kamil2002323@gmail.com>
2024-05-25 15:29:49 +0200

    Add Service side
    Added service side as WCF library

---
+++ WcfServiceLibrary1/WcfServiceLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ActiveUsersServiceLibrary
+{
+    public class ActiveUsersService : IActiveUsersService
+    {
+        private static readonly List<string> activeUsers = new List<string>();
+
+        public List<string> GetActiveUsers()
+        {
+            return activeUsers.ToList();
+        }
+
+        public void AddActiveUser(string username)
+        {
+            if (!activeUsers.Contains(username))
+            {
+                activeUsers.Add(username);
+            }
+        }
+
+        public void RemoveActiveUser(string username)
+        {
+            if (activeUsers.Contains(username))
+            {
+                activeUsers.Remove(username);
+            }
+        }
+    }
+}
